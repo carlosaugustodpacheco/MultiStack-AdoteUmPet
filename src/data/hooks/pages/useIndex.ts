572c9d61cebf754243d0320c255d42a36d3d4ca5
Carlos Augusto D. Pacheco <carlos.pacheco@aluno.ifsp.edu.br>
2022-06-07 01:05:24 -0300

    ajustes finais conexao back

--- src/data/hooks/pages/useIndex.ts
+++ src/data/hooks/pages/useIndex.ts
@@ -1,31 +1,59 @@
-import { useState } from "react";
+import { Axios, AxiosError } from "axios";
+import { useState, useEffect } from "react";
 import { Pet } from '../../@types/Pet';
-
+import { ApiService } from "../../services/ApiService";
 
 export function useIndex(){
-    const [listaPets, setListaPets] = useState(
-        [
-            {
-              id: 1,
-              nome: 'Kleber',
-              historia: 'lorem aeuhaeuzzzzzzzzzzzzzzzzzzzzzzzzaheuae',
-              foto: 'https://1.bp.blogspot.com/-SURP443Icuc/XYEWuD1SLzI/AAAAAAABOIQ/uuATPianOf8N5F_9NZYzY1lRECiM02ejQCLcBGAsYHQ/s1600/cats.jpg'
-            },
-            {
-              id: 2,
-              nome: 'Geraldo',
-              historia: 'lorem aeuhaeuaheuaeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-              foto: 'https://1.bp.blogspot.com/-SURP443Icuc/XYEWuD1SLzI/AAAAAAABOIQ/uuATPianOf8N5F_9NZYzY1lRECiM02ejQCLcBGAsYHQ/s1600/cats.jpg'
-            },
-
-          ]
-    ),
+    const [listaPets, setListaPets] = useState<Pet[]>([]),
           [petSelecionado, setPetSelecionado] = useState<Pet | null>(null),
           [email, setEmail] = useState(''),
           [valor, setValor] = useState(''),
           [mensagem, setMensagem] = useState('');
 
+    useEffect(() => {
+      ApiService.get('/pets')
+        .then((resposta) => {
+        setListaPets(resposta.data);
+        })
+    }, [])
+
+    useEffect(() => {
+      if(petSelecionado === null){
+        limparFormulario();
+      }
+    }, [petSelecionado])
+
     function adotar(){
+      if(petSelecionado !== null){
+        if(validarDadosAdocao()){
+          ApiService.post('/adocoes', {
+            pet_id: petSelecionado.id,
+            email,
+            valor
+          })
+            .then(() => {
+              setPetSelecionado(null);
+              setMensagem('Pet adotado com sucesso!');
+              // limparFormulario();
+            })
+            .catch((error: AxiosError) => {
+               // setMensagem(error.response?.data.message);
+            })
+        } else {
+          setMensagem('Preencha todos os campos corretamente!')
+        }
+
+      }
+
+    }
+
+    function validarDadosAdocao(){
+      return email.length > 0 && valor.length > 0;
+    }
+
+    function limparFormulario() {
+      setEmail('');
+      setValor('');
 
     }
 
